e7555042fdd5ff3252de6a71ca893100ebbff23d
Ni Kang <konie821@gmail.com>
2023-11-29 01:57:48 +0800

    finished creating booking, editing event and update event functions.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  has_many_attached :images
   validates :title, :description, :start_at, :end_at, :location, presence: true
 end

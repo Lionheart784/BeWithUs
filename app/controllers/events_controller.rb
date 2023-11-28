898bbec847f1c5c9ee64529e0fa2b3b4adc06385
Ni Kang <konie821@gmail.com>
2023-11-28 15:03:10 +0800

    event page and add event page created

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,26 +5,42 @@ class EventsController < ApplicationController
   end
 
   def show
-
+    @event = Event.find(params[:id])
   end
 
   def new
-
+    @event = Event.new
   end
 
   def create
-
+    @event = current_user.events.build(event_params)
+    if @event.save
+      redirect_to @event
+    else
+      render :new
+    end
   end
 
   def edit
-
+    @event = Event.find(params[:id])
   end
 
   def update
-
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      render :edit
+    end
   end
 
   def destroy
 
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :location, :description, :start_at, :end_at)
   end
 end
